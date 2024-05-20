49edcebbefd7491d9f2309c152e2a884055e85bd
Petr <nedryshkin11@mail.ru>
2024-05-20 17:47:19 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+data = {
+    "days_of_week": [
+        {"id": 1, "name": "Понедельник"},
+        {"id": 2, "name": "Вторник"},
+        {"id": 3, "name": "Среда"},
+        {"id": 4, "name": "Четверг"},
+        {"id": 5, "name": "Пятница"},
+        {"id": 6, "name": "Суббота"},
+        {"id": 7, "name": "Воскресенье"},
+    ]
+}
+
+@app.route('/', methods=['GET'])
+def get_users():
+    return jsonify(data["days_of_week"])
+
+
+
+if __name__ == '__main__':
+    app.run()
